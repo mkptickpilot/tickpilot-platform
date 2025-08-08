88f8d51e3a150195b1e52f4fe98f36a7db7e03fe
annieaa22 <annieaa222@gmail.com>
2025-08-08 12:40:55 +0530

    feat: added new error code

--- src/main/java/com/mkp/tickpilot/platform/util/Constants.java
+++ src/main/java/com/mkp/tickpilot/platform/util/Constants.java
@@ -4,9 +4,10 @@ public class Constants {
 
     public enum ERROR_CODES {
 
-        INVALID_AUTH_TOKEN("Invalid auth token"),
-        INVALID_EMAIL("Invalid Email"), 
-		INVALID_PASSWORD("Invalid Password");
+        INVALID_AUTH_TOKEN("Invalid auth token."),
+        INVALID_EMAIL("Invalid Email."), 
+		INVALID_PASSWORD("Invalid Password."),
+    	EMAIL_ALREADY_EXIST("Email already exist.");
 
         private final String message;
 
